7c5856044bf3d95f4dc5aad93c2c1f6c490c78ce
run532 <532499602@qq.com>
2014-01-12 14:57:58 +0800

    微博首页布局完成

--- weibo/controllers/welcome.php
+++ weibo/controllers/welcome.php
@@ -19,7 +19,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('signup');
+		$this->load->view('home');
 	}
 }
 
---
+++ weibo/views/home.php
@@ -0,0 +1,228 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>我的首页 新浪微博-随时随地分享身边的新鲜事儿</title>
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/reset.css') ?>">
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/style.css') ?>">
+	<script src="<?php echo base_url('assets/js/jquery-1.8.2.min.js') ?>"></script>
+</head>
+<body class="body_home">
+	<div class="miniblog">
+		<div class="miniblog_fb">
+			<div class="header global_nav">
+				<div class="nav_bg">
+					<div class="container">
+						<div class="logo">
+							<a href=""></a>
+						</div>
+						<div class="nav clearfix">
+							<a href="#" class="current">首页</a>
+							<a href="#">热门<i class="arrow"></i></a>
+							<a href="#">游戏<i class="arrow"></i></a>
+							<a href="#">应用<i class="arrow"></i></a>
+						</div>
+						<div class="search">
+							<input type="text" name="searchInput" value="大家都在搜：香格里拉大火">
+							<a href="javascript:void(0)" class="btn"></a>
+						</div>
+
+
+						<ul class="user clearfix">
+							<li class="username" ><a href="javascript:void(0)">air_zhe</a></li>
+							<li class="editor" ><a href="javascript:void(0)"><i></i></a></li>
+							<li class="msg" >
+								<a href="javascript:void(0)"><i></i></a>
+								<ul>
+									<li><a href="#"></a></li>
+									<li><a href="#"></a></li>
+									<li><a href="#"></a></li>
+								</ul>
+							</li>
+							<li class="setting" ><a href="javascript:void(0)"><i></i></a></li>
+							<li class="member" ><a href="javascript:void(0)"><i></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="container main">
+				<div class="content clearfix">
+					<div class="left_nav left">
+						<ul>
+							<li><a href="#"><i class="ico_myhomepage"></i>首页<i class="new"></i></a></li>
+							<li><a href="#"><i class="ico_message"></i>消息</a></li>
+							<li><a href="#"><i class="ico_favor"></i>收藏</a></li>
+							<li><a href="#"><i class="ico_sendtome"></i>发给我的</a></li>
+						</ul>
+						<ul class="goodfriend">
+							<li><a href=""><i class="ico_goodfriend"></i>好友圈</a></li>
+							<li></li>
+						</ul>
+						<div class="group">
+							<fieldset>
+								<legend class="S_txt2">分组</legend>
+							</fieldset>
+							<ul>
+								<li><a href="#"><i class="ico_group"></i>同事</a></li>
+								<li><a href="#"><i class="ico_group"></i>同学</a></li>
+								<li><a href="#"><i class="ico_group"></i>朋友</a></li>
+								<li><a href="#"><i class="ico_group"></i>家人</a></li>
+								<li><a href="#"><i class="ico_group"></i>明星</a></li>
+							</ul>
+						</div>
+
+					</div>
+					<div class="main clearfix">
+						<div class="box_center left">
+							<div>
+								<div class="send_weibo">
+									<div class="title_area">
+										有什么新鲜事想告诉大家？
+										<s></s>
+									</div>
+									<div class="input clicked">
+										<textarea name="" title="微博输入框"  class="input_detail" id="" cols="30" rows="10"></textarea>
+										<span class="arrow"></span>
+										<div class="send_succpic">
+											<p class="icon_succB"></p>
+											<p class="txt">发布成功</p>
+										</div>
+									</div>
+									<div class="func_area clearfix">
+										<div class="kind_detail left">
+											<a href="javascript:void(0)"><i class="icon_face"></i>表情</a>
+											<a href="javascript:void(0)"><i class="icon_img"></i>图片</a>
+											<a href="javascript:void(0)"><i class="icon_video"></i>视频</a>
+											<a href="javascript:void(0)"><i class="icon_qing"></i>话题</a>
+											<a href="javascript:void(0)"><i class="icon_chang"></i>长微博</a>
+										</div>
+										<a href="" class="send_btn right "><span>发布</span></a>
+									</div>
+									<div class="weibo">
+										<div class="title">
+											<h3>微博</h3>
+											<div class="bg"></div>
+											<p class="weibo_type">
+												<a href="#" class="active">全部</a>
+												<a href="#">原创</a>
+												<a href="#">图片</a>
+												<a href="#">视频</a>
+												<a href="#">音乐</a>
+											</p>
+										</div>
+										<div class="weibo_list">
+											<a href="" class="notes">有 1 条新微博，点击查看</a>
+											<div class="item clearfix">
+												<div class="face">
+													<img  width="50" height="50" src="<?php echo base_url('assets/images/1.jpg') ?>" alt="">
+												</div>
+												<div class="detail">
+													<div><a class="name" href="#">小殷爱录像</a></div>
+													<div class="content">
+														从前有个国王,他两个女儿的眼泪都会变成钻石.大女儿嫁给了一个用她的眼泪创造了一个个城堡的王子,小女儿却嫁给了牧羊人.国王临死见到他们的时候,大女儿满身金银珠宝,而小女儿和牧羊人仍是贫穷.国王很惊讶的说:明明她的一滴眼泪就够你们生活的很好. 牧羊人说:可是我舍不得让她哭啊.....
+													</div>
+													<div class="media_prev">
+														<ul>
+															<li>
+																<a href="javascript:void(0)"><img src="<?php echo base_url('assets/images/7cff6573jw1ebibx4063cj20c60fhgmn.jpg') ?>" alt=""></a>
+															</li>
+														</ul>
+													</div>
+													<div class="func clearfix S_txt2">
+														<div class="from left"><a href="#" class="time">今天 07:48</a> 来自<a href="">新浪微博</a> </div>
+														<div class="handle right"><a href=""><s class="icon_praised_b"></s>(29)</a><i class="S_txt3">|</i><a href="">转发(71)</a><i class="S_txt3">|</i><a href="">收藏</a><i class="S_txt3">|</i><a href="">评论(22)</a></div>
+													</div>
+												</div>
+											</div>
+											<div class="item clearfix">
+												<div class="face">
+													<img  width="50" height="50" src="<?php echo base_url('assets/images/2.jpg') ?>" alt="">
+												</div>
+												<div class="detail">
+													<div><a class="name" href="#">包子包子肉肉</a></div>
+													<div class="content">
+														转发微博
+													</div>
+													<div class="forwardContent"> 
+														<div><a class="name" href="#">@收集世上的美景</a></div>
+														<div class="content">
+															【中国最美五大沙漠】巴丹吉林沙漠，塔克拉玛干沙漠，鸣沙山—月牙泉，古尔班通古特沙漠，沙坡头。一生一定要去次沙漠，体验烈日风沙，体味孤独辽远，它在那里等你，等候了千年。什么时候启程吧！
+														</div>	
+														<div class="media_prev">
+															<ul>
+																<li>
+																	<a href="javascript:void(0)"><img src="<?php echo base_url('assets/images/c31ab1f2jw1ecfxy8kvhej20c811xaga.jpg') ?>" alt=""></a>
+																</li>
+															</ul>
+														</div>
+														<div class="func clearfix S_txt2">
+															<div class="from left"><a href="#" class="time">1月11日 23:01</a> 来自<a href="">pull</a> </div>
+															<div class="handle right"><a href=""><s class="icon_praised_b"></s>(19)</a><i class="S_txt3">|</i><a href="">转发(71)</a><i class="S_txt3">|</i><a href="">收藏</a><i class="S_txt3">|</i><a href="">评论(22)</a></div>
+														</div>
+													</div>
+													
+													<div class="func clearfix S_txt2">
+														<div class="from left"><a href="#" class="time">今天 07:48</a> 来自<a href="">新浪微博</a> </div>
+														<div class="handle right"><a href=""><s class="icon_praised_b"></s>(29)</a><i class="S_txt3">|</i><a href="">转发(71)</a><i class="S_txt3">|</i><a href="">收藏</a><i class="S_txt3">|</i><a href="">评论(22)</a></div>
+													</div>
+												</div>
+											</div>
+										</div>
+
+									</div>
+								</div>
+
+							</div>
+						</div>
+						<div class="box_right right">
+							<a class="set_skin" href="#" title="模板设置"></a>
+							<div class="user_info">
+								<img  width="80" height="80" src="<?php echo base_url('assets/images/avatar.jpg') ?>" alt="">
+								<div>
+									<a href="" class="username">run5324</a>
+									<br>
+									<a href="" class="level"><span></span></a>
+								</div>
+							</div>
+							<ul class="user_atten clearfix">
+								<li>
+									<a href="">
+										<strong>35</strong>
+										<span>关注</span>
+									</a>
+								</li>
+								<li>
+									<a href="">
+										<strong>8</strong>
+										<span>粉丝</span>
+									</a>
+								</li>
+								<li>
+									<a href="">
+										<strong>42</strong>
+										<span>微博</span>
+									</a>
+								</li>
+							</ul>
+							<div class="hot_topics">
+								<fieldset>
+									<legend class="S_txt2"><a href="#">热门话题</a></legend>
+								</fieldset>
+								<ul>
+									<li><a href="#">#汤唯遭遇电信诈骗#</a><span class="total S_txt2">2634万</span></li>
+									<li><a href="#">#当时忍住就好了#</a><span class="total S_txt2">328万</span></li>
+									<li><a href="#">#爱情公寓4#</a><span class="total S_txt2">285</span></li>
+									<li><a href="#">#孙悟空后人#</a><span class="total S_txt2">3万</span></li>
+									<li><a href="#">#来自星星的你#</a><span class="total S_txt2">854万</span></li>
+									<li><a href="#">#中国好歌曲#</a><span class="total S_txt2">677</span></li>
+								</ul>
+							</div>		
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="global_footer"></div>
+		</div>
+	</div>
+</body>
+</html>
