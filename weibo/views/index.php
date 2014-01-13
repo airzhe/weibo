e7d6fed6f7f1b32f8a076ee76ac0e6a96385b639
run532 <532499602@qq.com>
2014-01-13 16:35:03 +0800

    备份

--- weibo/views/index.php
+++ weibo/views/index.php
@@ -129,7 +129,59 @@
 										今天考试，开卷我就蒙了，不会写，监考老师转圈，将近30分钟，我一个字没写，老师看看我，我看看老师。40分钟的时候，我卷子写满。
 									</div>
 									<div class="func clearfix S_txt2">
-										<div class="from left"><a href="#" class="time">今天 07:48</a> 来自<a href="">新浪微博</a> </div>
+										<div class="from left"><a href="#" class="time">今天 07:48</a> 来自<a href="">iPhone客户端</a> </div>
+										<div class="handle right"><a href=""><s class="icon_praised_b"></s>(29)</a><i class="S_txt3">|</i><a href="">转发(71)</a><i class="S_txt3">|</i><a href="">收藏</a><i class="S_txt3">|</i><a href="">评论(22)</a></div>
+									</div>
+								</div>
+							</div>
+							<div class="item">
+								<div class="detail">
+									<div class="content">
+										有几个是拼图的，这种小的尺寸全部是用一般的胶水冷压的，热激活胶水是粘超大型木皮用的。做这种其实不难，拼图案更复杂些。//<a href="#">@发粪涂墙的胖子</a>: 看起来要完贴皮啊 ，而且妥妥的手工制作了， 机器好像不怎么派得上用场。 <a href="#">@小殷爱录像</a> 说的那种热敏胶有卖的？
+									</div>
+									<div class="forwardContent"> 
+										<div><a class="name" href="#">@湖边的森林6</a></div>
+										<div class="content">
+											随着年龄的增长我觉得自己越来越稳重拉阿哈哈哈哈哈叉腰仰天长笑！！
+										</div>	
+										<div class="media_prev">
+											<ul class="lotspic_list clearfix">
+												<li>
+													<a href="javascript:void(0)"><img src="<?php echo base_url('assets/images/img1.jpg') ?>" alt=""></a>
+												</li>
+												<li>
+													<a href="javascript:void(0)"><img src="<?php echo base_url('assets/images/img2.jpg') ?>" alt=""></a>
+												</li>
+												<li>
+													<a href="javascript:void(0)"><img src="<?php echo base_url('assets/images/img3.jpg') ?>" alt=""></a>
+												</li>
+												<li>
+													<a href="javascript:void(0)"><img src="<?php echo base_url('assets/images/img4.jpg') ?>" alt=""></a>
+												</li>
+												<li>
+													<a href="javascript:void(0)"><img src="<?php echo base_url('assets/images/img5.jpg') ?>" alt=""></a>
+												</li>
+												<li>
+													<a href="javascript:void(0)"><img src="<?php echo base_url('assets/images/img6.jpg') ?>" alt=""></a>
+												</li>
+												<li>
+													<a href="javascript:void(0)"><img src="<?php echo base_url('assets/images/img7.jpg') ?>" alt=""></a>
+												</li>
+												<li>
+													<a href="javascript:void(0)"><img src="<?php echo base_url('assets/images/img8.jpg') ?>" alt=""></a>
+												</li>
+												<li>
+													<a href="javascript:void(0)"><img src="<?php echo base_url('assets/images/img9.jpg') ?>" alt=""></a>
+												</li>
+											</ul>
+										</div>
+										<div class="func clearfix S_txt2">
+											<div class="from left"><a href="#" class="time">1月11日 23:01</a> 来自<a href="">pull</a> </div>
+											<div class="handle right"><a href=""><s class="icon_praised_b"></s>(19)</a><i class="S_txt3">|</i><a href="">转发(71)</a><i class="S_txt3">|</i><a href="">收藏</a><i class="S_txt3">|</i><a href="">评论(22)</a></div>
+										</div>
+									</div>
+									<div class="func clearfix S_txt2">
+										<div class="from left"><a href="#" class="time">今天 07:48</a> 来自<a href="">iPhone客户端</a> </div>
 										<div class="handle right"><a href=""><s class="icon_praised_b"></s>(29)</a><i class="S_txt3">|</i><a href="">转发(71)</a><i class="S_txt3">|</i><a href="">收藏</a><i class="S_txt3">|</i><a href="">评论(22)</a></div>
 									</div>
 								</div>
