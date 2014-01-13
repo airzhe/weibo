1a3f2bed613ef2abab79cfac291deac48ec6330d
run532 <532499602@qq.com>
2014-01-13 12:58:43 +0800

    完成个人首页和消息页面

--- weibo/controllers/welcome.php
+++ weibo/controllers/welcome.php
@@ -19,7 +19,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('home');
+		$this->load->view('msg');
 	}
 }
 
---
+++ weibo/views/index.php
@@ -0,0 +1,157 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>airzhe的微博|新浪微博-随时随地分享身边的新鲜事儿</title>
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/reset.css') ?>">
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/style.css') ?>">
+	<script src="<?php echo base_url('assets/js/jquery-1.8.2.min.js') ?>"></script>
+</head>
+<body class="body_index">
+	<div class="miniblog">
+		<div class="miniblog_fb">
+			<div class="header global_nav">
+				<div class="nav_bg">
+					<div class="container">
+						<div class="logo">
+							<a href=""></a>
+						</div>
+						<div class="nav clearfix">
+							<a href="#" class="current">首页</a>
+							<a href="#">热门<i class="arrow"></i></a>
+							<a href="#">游戏<i class="arrow"></i></a>
+							<a href="#">应用<i class="arrow"></i></a>
+						</div>
+						<div class="search">
+							<input type="text" name="searchInput" value="大家都在搜：香格里拉大火">
+							<a href="javascript:void(0)" class="btn"></a>
+						</div>
+
+
+						<ul class="user clearfix">
+							<li class="username" ><a href="javascript:void(0)">air_zhe</a></li>
+							<li class="editor" ><a href="javascript:void(0)"><i></i></a></li>
+							<li class="msg" >
+								<a href="javascript:void(0)"><i></i></a>
+								<ul>
+									<li><a href="#"></a></li>
+									<li><a href="#"></a></li>
+									<li><a href="#"></a></li>
+								</ul>
+							</li>
+							<li class="setting" ><a href="javascript:void(0)"><i></i></a></li>
+							<li class="member" ><a href="javascript:void(0)"><i></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="profile_pic_top" style="background-image:url(<?php echo base_url('assets/images/015.jpg') ?>)">
+					<a class="set_skin" href="#" title="模板设置"></a>
+				</div>
+				<div class="user_info clearfix">
+					<div class="head left">
+						<img class="avatar" src="./assets/images/avatar.jpg" alt="">
+						<ul class="user_atten clearfix">
+							<li>
+								<a href="">
+									<strong>35</strong>
+									<span>关注</span>
+								</a>
+							</li>
+							<li>
+								<a href="">
+									<strong>8</strong>
+									<span>粉丝</span>
+								</a>
+							</li>
+							<li>
+								<a href="">
+									<strong>42</strong>
+									<span>微博</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="info">
+						<div>
+							<span class="name">airzhe</span> 
+							<a href="" class="level"><span></span></a>
+							<a href="#">http://weibo.com/airzhe</a>
+						</div>
+						<p class="S_txt2">分手快乐</p>
+						<p><em class="male"></em><i>|</i><a href="#">求交往</a><i>|</i><a href="">河南 郑州</a><i>|</i></p>
+						<p class="edit"><a href=""><span>编辑个人资料</span></a></p>
+					</div>
+					
+				</div>
+				<div >
+					<ul class="core_nav clearfix">
+						<li class="current"><a href="#">我的主页</a></li>
+						<li><a href="#">微博</a></li>
+						<li><a href="#">个人资料</a></li>
+						<li><a href="#">相册</a></li>
+						<li><a href="#">赞</a></li>
+						<li><a href="#">足迹</a></li>
+						<li><a href="#">更多</a></li>
+					</ul>
+				</div>
+				<div class="wrap">
+					<div class="main">
+						<p class="tab_radious">
+							<a href="#" class="active"><strong>全部</strong><span class="tabarrow"></span></a>
+							<i class="S_txt2">|</i>
+							<a href="#">原创</a><i class="S_txt3">|</i>
+							<a href="#">图片</a>
+						</p>
+						<div class="feed_self" style="height:1200px;background:#fff;">
+							<div class="item">
+								<div class="detail">
+									<div class="content">
+										这个很适合我，我的小马好累吖
+									</div>
+									<div class="media_prev">
+										<ul>
+											<li>
+												<a href="javascript:void(0)"><img src="<?php echo base_url('assets/images/abb9e8d9jw1ec436y9swrj20a008s74i.jpg') ?>" alt=""></a>
+											</li>
+										</ul>
+									</div>
+									<div class="func clearfix S_txt2">
+										<div class="from left"><a href="#" class="time">今天 07:48</a> 来自<a href="">新浪微博</a> </div>
+										<div class="handle right"><a href=""><s class="icon_praised_b"></s>(29)</a><i class="S_txt3">|</i><a href="">转发(71)</a><i class="S_txt3">|</i><a href="">收藏</a><i class="S_txt3">|</i><a href="">评论(22)</a></div>
+									</div>
+								</div>
+							</div>
+							<div class="item">
+								<div class="detail">
+									<div class="content">
+										今天考试，开卷我就蒙了，不会写，监考老师转圈，将近30分钟，我一个字没写，老师看看我，我看看老师。40分钟的时候，我卷子写满。
+									</div>
+									<div class="func clearfix S_txt2">
+										<div class="from left"><a href="#" class="time">今天 07:48</a> 来自<a href="">新浪微博</a> </div>
+										<div class="handle right"><a href=""><s class="icon_praised_b"></s>(29)</a><i class="S_txt3">|</i><a href="">转发(71)</a><i class="S_txt3">|</i><a href="">收藏</a><i class="S_txt3">|</i><a href="">评论(22)</a></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="relation">
+						<h4>微关系</h4>
+						<span>我们之间的共同关系</span>
+					</div>
+				</div>
+				<!-- 返回顶部 -->
+				<a class="gotop S_txt2" href="javascript:void(0);">
+					<span>
+						<s class="icon_gotop"></s>
+						返回顶部
+					</span>
+				</a>
+				<!-- 返回顶部结束 -->
+			</div>
+			<div class="global_footer"></div>
+		</div>
+	</div>
+</body>
+</html>
