b421114afd5a8098cfbf9535f7d9e57cb45654cf
run532 <532499602@qq.com>
2014-01-17 19:16:12 +0800

    添加css皮肤风格

--- weibo/views/home.php
+++ weibo/views/home.php
@@ -280,7 +280,48 @@
 				</a>
 				<!-- 返回顶部结束 -->
 			</div>
-			<div class="global_footer"></div>
+			<div class="global_footer">
+				<div class="clearfix">
+					<dl class="list">
+						<dt>找找感兴趣的人</dt>
+						<dd><a href="#">名人堂</a><a href="#">微博会员</a></dd>
+						<dd><a href="#">微博达人</a></dd>
+						<dd><a href="#">媒体</a><a href="#">企业</a><a href="#">政府</a></dd>
+					</dl>
+					<dl class="list">
+						<dt>精彩内容</dt>
+						<dd><a href="#">微话题</a><a href="#">风云榜</a></dd>
+						<dd><a href="#">微访谈</a></dd>
+						<dd><a href="#">热门微博</a></dd>
+					</dl>
+					<dl class="list">
+						<dt>热门应用</dt>
+						<dd><a href="#">微公益</a><a href="#">微吧</a></dd>
+						<dd><a href="#">微音乐</a><a href="#">相册</a></dd>
+						<dd><a href="#">微游戏</a><a href="#">微博商学院</a></dd>
+					</dl>
+					<dl class="list"> 
+						<dt>手机玩微博</dt>
+						<dd><a href="#">WAP版</a><a href="#">短彩发微博</a></dd>
+						<dd><a href="#">iPhone客户端</a><a href="#">iPad客户端</a></dd>
+						<dd><a href="#">Android客户端</a></dd>
+					</dl>
+					<dl class="list">
+						<dt>认证&amp;合作</dt>
+						<dd><a href="#">申请认证</a><a href="#">开放平台</a></dd>
+						<dd><a href="#">企业微博</a><a href="#">连接网站</a></dd>
+						<dd><a href="#">微博标识</a><a href="#">广告服务</a></dd>
+					</dl>
+				</div>
+				<div class="copyright S_line2">
+					<p><i class="W_ico16 ico_weibo"></i>微博帮助　 意见反馈　 开放平台　 微博招聘　 新浪网导航 社区管理中心　 微博社区公约</p>
+					<p class="S_txt2">客服电话：4000 960 960（个人） 4000 980 980（企业） （按当地市话标准计算）</p>
+					<p>
+						<span class="S_txt2">北京微梦创科网络技术有限公司</span> <a href="#">京网文[2011]0398-130号</a> <a href="#">京ICP证100780号</a>
+						<span class="right">Copyright © 1996-2014 SINA</span>
+					</p>
+				</div>
+			</div>
 		</div>
 	</div>
 </body>
