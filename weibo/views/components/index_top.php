86a7747031179de773aec64fa75a1ea3c3b067d8
run532 <532499602@qq.com>
2014-01-18 19:14:31 +0800

    增加赞模板

---
+++ weibo/views/components/index_top.php
@@ -0,0 +1,52 @@
+<div class="profile_pic_top" style="background-image:url(<?php echo base_url('assets/images/015.jpg') ?>)">
+	<a class="set_skin" href="#" title="模板设置"></a>
+</div>
+<div class="user_info clearfix">
+	<div class="head left">
+		<div class="avatar">
+			<img src="./assets/images/avatar.jpg" alt="">
+			<a href="#" class="W_btn_c change_avatar"><span>更换头像</span></a>
+		</div>
+		<ul class="user_atten clearfix">
+			<li class="S_line1">
+				<a href="" class="S_func1">
+					<strong>35</strong>
+					<span>关注</span>
+				</a>
+			</li>
+			<li class="S_line1">
+				<a href="" class="S_func1">
+					<strong>8</strong>
+					<span>粉丝</span>
+				</a>
+			</li>
+			<li class="S_line1">
+				<a href="" class="S_func1">
+					<strong>42</strong>
+					<span>微博</span>
+				</a>
+			</li>
+		</ul>
+	</div>
+	<div class="info">
+		<div>
+			<span class="name">airzhe</span> 
+			<a href="" class="W_level_ico color3"><span class="W_level_num l7"></span></a>
+			<a href="#">http://weibo.com/airzhe</a>
+		</div>
+		<p class="S_txt2">分手快乐</p>
+		<p><i class="W_ico12 male"></i><i class="W_vline S_line1_c">|</i><a href="#">求交往</a><i class="W_vline S_line1_c">|</i><a href="">河南 郑州</a></p>
+		<p class="edit"><a href="" class="W_btn_c"><span>编辑个人资料</span></a></p>
+	</div>
+</div>
+<div >
+	<ul class="core_nav clearfix">
+		<li class="current"><a href="#">我的主页</a></li>
+		<li><a href="#">微博</a></li>
+		<li><a href="#">个人资料</a></li>
+		<li><a href="#">相册</a></li>
+		<li><a href="#">赞</a></li>
+		<li><a href="#">足迹</a></li>
+		<li><a href="#">更多</a></li>
+	</ul>
+</div>
