547e605ada7731fc89e50186841736d8f668fcb4
run532 <532499602@qq.com>
2014-03-04 00:04:25 +0800

    Backup

---
+++ weibo/logs/log-2014-03-04.php
@@ -0,0 +1,21 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-03-04 00:01:37 --> 404 Page Not Found --> images
+ERROR - 2014-03-04 00:01:42 --> 404 Page Not Found --> images
+ERROR - 2014-03-04 00:02:10 --> 404 Page Not Found --> images
+ERROR - 2014-03-04 00:02:15 --> 404 Page Not Found --> assets
+ERROR - 2014-03-04 00:02:24 --> Severity: Notice  --> Undefined index: style /home/runner/www/work/weibo/weibo/models/user_model.php 61
+ERROR - 2014-03-04 00:02:25 --> 404 Page Not Found --> images
+ERROR - 2014-03-04 00:02:27 --> 404 Page Not Found --> images
+ERROR - 2014-03-04 00:02:28 --> 404 Page Not Found --> images
+ERROR - 2014-03-04 00:02:29 --> 404 Page Not Found --> images
+ERROR - 2014-03-04 00:02:41 --> 404 Page Not Found --> images
+ERROR - 2014-03-04 00:02:45 --> 404 Page Not Found --> images
+ERROR - 2014-03-04 00:03:01 --> Severity: Notice  --> Undefined index: style /home/runner/www/work/weibo/weibo/models/user_model.php 61
+ERROR - 2014-03-04 00:03:01 --> 404 Page Not Found --> images
+ERROR - 2014-03-04 00:03:04 --> 404 Page Not Found --> images
+ERROR - 2014-03-04 00:03:06 --> 404 Page Not Found --> images
+ERROR - 2014-03-04 00:03:45 --> Severity: Notice  --> Undefined index: style /home/runner/www/work/weibo/weibo/models/user_model.php 61
+ERROR - 2014-03-04 00:03:46 --> 404 Page Not Found --> images
+ERROR - 2014-03-04 00:03:47 --> 404 Page Not Found --> images
+ERROR - 2014-03-04 00:03:50 --> 404 Page Not Found --> images
